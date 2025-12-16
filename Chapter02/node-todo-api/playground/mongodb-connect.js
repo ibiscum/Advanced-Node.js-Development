d52e6c758b3f5a8cb2229a3ef94e4945a83556d8
ibiscum <ulf.luecking@gmail.com>
2025-12-17 00:32:48 +0100

    ch02

--- Chapter02/node-todo-api/playground/mongodb-connect.js
+++ Chapter02/node-todo-api/playground/mongodb-connect.js
@@ -1,5 +1,6 @@
-const {MongoClient} = require('mongodb');
+import pkg from 'mongodb';
 
+const {MongoClient} = pkg;
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   if(err){
     return console.log('Unable to connect MongoDB server');
